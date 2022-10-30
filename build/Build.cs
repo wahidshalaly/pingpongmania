65e8f04b85e1ceff78500502fa2af7ff20fa802c
Wahid Shalaly <194151+wahidshalaly@users.noreply.github.com>
2022-10-30 18:22:01 +1100

    Configure versioning and initial building script

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,54 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Serilog;
+using Nuke.Common.Tools.NerdbankGitVersioning;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    private readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    private readonly Solution Solution;
+
+    [NerdbankGitVersioning]
+    private readonly NerdbankGitVersioning NerdbankGitVersioning;
+
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(_ => _.SetProject(Solution));
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(_ => _.SetProjectFile(Solution));
         });
+
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
 
+    Target Print => _ => _
+        .Executes(() =>
+        {
+            Log.Information("Solution: {Value}", Solution);
+            Log.Information("SemVer2: {Value}", NerdbankGitVersioning.CloudBuildNumber);
+        });
 }
